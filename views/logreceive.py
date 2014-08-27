461c3dc165b20c4661e50ccd5898019e0bad6c4c
Pete Matern <pete@jivesoftware.com>
2014-08-27 15:47:57 -0700

    reading snappy compressed json log messages

--- views/logreceive.py
+++ views/logreceive.py
@@ -1,10 +1,37 @@
+import snappy
+import json
 from flask import Blueprint, request
+
 
 logreceive = Blueprint('logreceive', __name__)
 
 
 @logreceive.route('/log/receive/events/snappy', methods=['POST'])
 def snappy_compressed_events():
-    request.stream
+    entry_bytes = snappy.uncompress(request.data)
+    json_logs = parse_log_entries(entry_bytes)
+    send_to_mongo(json_logs)
 
     pass
+
+def parse_log_entries(entry_bytes):
+    idx = 0
+    msgs = []
+
+    while idx < len(entry_bytes):
+        size = build_int(entry_bytes[idx : idx + 4])
+        idx += 4
+        json_bytes = entry_bytes[idx : idx + size]
+        idx += size
+
+        json_object = json.loads(json_bytes)
+        msgs.append(json_object)
+
+    return msgs
+
+
+def send_to_mongo(json_logs):
+    print("mongo sees %s" % json_logs)
+
+def build_int(four_bytes):
+    return int(four_bytes.encode('hex'), 16)

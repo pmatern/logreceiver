b5c36171ce17c3c8f542d2c808428cf815d6e025
Pete Matern <pete@jivesoftware.com>
2014-08-27 21:40:28 -0700

    got mongo working and wired in tornado

--- views/logreceive.py
+++ views/logreceive.py
@@ -1,5 +1,6 @@
 import snappy
 import json
+from pymongo import MongoClient
 from flask import Blueprint, request
 
 logreceive = Blueprint('logreceive', __name__)
@@ -7,10 +8,12 @@ logreceive = Blueprint('logreceive', __name__)
 @logreceive.route('/log/receive/events', methods=['POST'])
 def receive_events():
     parse_and_store(request.data)
+    return "OK", 200
 
 @logreceive.route('/log/receive/events/snappy', methods=['POST'])
 def receive_snappy_compressed_events():
     parse_and_store(snappy.uncompress(request.data))
+    return "OK", 200
 
 def parse_and_store(entry_bytes):
     send_to_mongo(parse_log_entries(entry_bytes))
@@ -32,7 +35,11 @@ def parse_log_entries(entry_bytes):
 
 
 def send_to_mongo(json_logs):
-    print("mongo sees %s" % json_logs)
+    client = MongoClient("localhost", 27017) # yeah I know
+    db = client.metric_logs # yeah I know
+    collection = db.wal
+    collection.insert(json_logs)
+
 
 def build_int(four_bytes):
     return int(four_bytes.encode('hex'), 16)
